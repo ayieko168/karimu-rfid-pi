60adeac24ccc65ef09f2be5c7b4c61dc26bafa98
Antony Alen <antonyalen1960@gmail.com>
2021-09-25 10:40:40 +0300

    Update v3.1 on deploy

--- NEW/v3.1.py
+++ NEW/v3.1.py
@@ -5,14 +5,34 @@ import concurrent.futures
 import requests
 import datetime
 import time
+import threading
+import logging
+import logging.handlers
+import os
+
+
+## setup logger
+logger = logging.getLogger(__name__)
+fl_handler = logging.FileHandler('kenha_rfid.log')
+fl_handler.setLevel(logging.DEBUG)
+fl_format = logging.Formatter('%(asctime)s - %(name)s - %(levelname)s - %(message)s')
+fl_handler.setFormatter(fl_format)
+logger.addHandler(fl_handler)
+
 
 ## Variables
-COMPORT = 'COM4'
-url = "http://192.168.0.105:105/car" 
+devices = [
+    {"COMPORT": 'COM25', "machine_id": "gB6EMj3aTw01"},
+    {"COMPORT": 'COM4', "machine_id": "8GNeUCBU4b02"},
+]
+# url = "http://127.0.0.1:105/car" 
+url = "http://192.168.1.2/api/Events/rfid/read"
 read_tags = {}
 time_delta = int(6e+10) * 0.5  ## Check time in minutes
+connection_timeout = 30  ## Time in seconds to timeout
 
 print(time_delta)
+logger.debug("Starting RFID listener..")
 
 def list_available_ports():
     
@@ -60,48 +80,71 @@ def chech_2x(tag_id):
     
     
 def send_tag_data(tag_id):  
+    
+    print(f"Sending tag :: {tag_id}")
+    
+    try:
         
-    r = requests.post(url, json={'laneNumber': 1, 'tagNumber': str(tag_id), 'dateTime': str(datetime.datetime.now())})
-    print((tag_id, r.status_code, r.reason))
+        conext = {
+          "readerNumber": "string",
+          "tagNumber": "string"
+        }
+        
+        r = requests.post(url, json=conext, timeout=connection_timeout)
+        print((tag_id, r.status_code, r.reason))
+    
+    except Exception as e:
+        
+        print(f"Error sending ID to server :: {e}")
 
+## Run the serial listenner
+def listener(device):
+    
+    try:
+        print(f"Starting listener [{device}]...")
+        ser =  serial.Serial(device["COMPORT"])
+        
+        print(f"Listenner started, {ser}\n")
+        print("#"*50)
 
-try:
-    ser =  serial.Serial(COMPORT)
-
-    while True:
-        
-        
-        ## Get the Data from the reader
-        read_raw = ser.readline()
-        
-        # print(read_raw)
-        data = read_raw.decode('UTF-8')
-        clean_data = data[2:].strip()
-        
-        # print(clean_data, len(clean_data))
-        
-        ## Check if length of read ID is valid
-        if len(clean_data) == 10:
-            print("OK!!")
+        while True:
             
-            ## Check the twice thing
-            ret = chech_2x(clean_data)
             
-            ## Send the ID data to the server
-            if ret:
-                print(read_tags, clean_data)
-                with concurrent.futures.ThreadPoolExecutor(max_workers=15) as executor:
-                    executor.map(send_tag_data, [clean_data])
+            ## Get the Data from the reader
+            read_raw = ser.readline()
+            
+            # print(read_raw)
+            data = read_raw.decode('UTF-8')
+            clean_data = data[2:].strip()
+            
+            # print(clean_data, len(clean_data))
+            
+            ## Check if length of read ID is valid
+            if len(clean_data) == 10:
+                print("OK!!")
+                
+                ## Check the twice thing
+                ret = chech_2x(clean_data)
+                print(f"Checked the twice thing :: {ret}")
+                
+                ## Send the ID data to the server
+                if ret:
+                    print(read_tags, clean_data)
+                    threading.Thread(target=send_tag_data, args=(clean_data,)).start()
+                    # with concurrent.futures.ThreadPoolExecutor(max_workers=15) as executor:
+                        # executor.map(send_tag_data, [clean_data])
 
 
-except Exception as e:
-    
-    print(f"Error!! :: {e}")
-    if 'FileNotFoundError' in str(e):
-        print('The port you decleraed is not connected, bellow is a list of active ports...')
-        list_available_ports()
-    
-    else:
-        list_available_ports()
+    except Exception as e:
         
-    
+        print(f"Error!! :: {e}")
+        if 'FileNotFoundError' in str(e):
+            print('The port you decleraed is not connected, bellow is a list of active ports...')
+            list_available_ports()
+        
+        else:
+            list_available_ports()
+                
+
+with concurrent.futures.ThreadPoolExecutor(max_workers=10) as executor2:
+    executor2.map(listener, devices)
